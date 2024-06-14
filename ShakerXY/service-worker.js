e844aa952ee0dd2ced6190a2febcc1de8d7c0d2e
Stephane Letz <letz@grame.fr>
2024-06-14 19:35:31 +0200

    Add PWA applications.

---
+++ ShakerXY/service-worker.js
@@ -0,0 +1,38 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('ShakerXY-static-v1').then(cache => {
+            console.log("Caching datas");
+            return cache.addAll([
+                './',
+                './faust-ui/index.js',
+                './faust-ui/index.css',
+                './faustwasm/index.js',
+                './index.html',
+                './ShakerXY.js',
+                './ShakerXY.wasm',
+                './ShakerXY.json',
+            ]).catch(error => {
+                // Catch and log any errors during the caching process
+                console.error('Failed to cache resources during install:', error);
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    // This ensures that the new service worker takes control immediately
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    console.log("event.request", event.request);
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            // Return the cached response if found, else fetch from network
+            return response || fetch(event.request).catch(() => {
+                // Fallback content or page for failed network requests
+                return caches.match('./offline.html');
+            });
+        })
+    );
+});
