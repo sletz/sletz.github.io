ccbaedbba5e194c4e91b0bf6cd4067baec1bea21
Stephane Letz <letz@grame.fr>
2024-06-15 11:50:34 +0200

    Regenerated.

--- AttacKey/service-worker.js
+++ AttacKey/service-worker.js
@@ -1,48 +1,5 @@
 
-
 const CACHE_NAME = 'AttacKey-static'; // Cache name without versioning
-
-/*
-// Function to request persistent storage
-async function requestPersistentStorage() {
-    if (navigator.storage && navigator.storage.persist) {
-        const isPersisted = await navigator.storage.persist();
-        console.log(`Persistent storage granted: ${isPersisted}`);
-    } else {
-        console.log('Persistent storage is not supported by this browser');
-    }
-}
-*/
-
-/*
-self.addEventListener('install', event => {
-    event.waitUntil(
-        (async () => {
-            // Request persistent storage
-            await requestPersistentStorage();
-            // Open the cache and add all required resources
-            const cache = await caches.open(CACHE_NAME);
-            console.log("Caching data");
-            try {
-                await cache.addAll([
-                    './',
-                    './faust-ui/index.js',
-                    './faust-ui/index.css',
-                    './faustwasm/index.js',
-                    './index.html',
-                    './AttacKey.js',
-                    './AttacKey.wasm',
-                    './AttacKey.json',
-                ]);
-            } catch (error) {
-                // Catch and log any errors during the caching process
-                console.error('Failed to cache resources during install:', error);
-            }
-        })()
-    );
-});
-*/
-
 
 self.addEventListener('install', event => {
     event.waitUntil(
@@ -64,7 +21,6 @@ self.addEventListener('install', event => {
     );
 });
 
-
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
@@ -81,40 +37,22 @@ self.addEventListener('activate', event => {
     );
 });
 
-/*
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            // Return the cached response if found, else fetch from network
-            return response || fetch(event.request).catch(() => {
-                // Fallback content or page for failed network requests
-                return caches.match('./offline.html');
-            });
-        })
-    );
-});
-*/
-
 self.addEventListener('fetch', event => {
     event.respondWith((async () => {
         const cache = await caches.open(CACHE_NAME);
-        console.log(cache);
-        // Get the resource from the cache.
-        console.log(event.request);
+        // Get the resource from the cache
         const cachedResponse = await cache.match(event.request);
-        console.log(cachedResponse);
-
         if (cachedResponse) {
             return cachedResponse;
         } else {
             try {
-                // If the resource was not in the cache, try the network.
+                // If the resource was not in the cache, try the network
                 const fetchResponse = await fetch(event.request);
-                // Save the resource in the cache and return it.
+                // Save the resource in the cache and return it
                 cache.put(event.request, fetchResponse.clone());
                 return fetchResponse;
             } catch (e) {
-                // The network failed.
+                // The network faile
                 console.log('Network access error', CACHE_NAME);
             }
         }
