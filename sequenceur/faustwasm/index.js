2a85c71292dc855d6c470ae76ff76dc8b6d2c08c
Stephane Letz <letz@grame.fr>
2024-10-01 17:32:44 +0200

    Add alert for motion and orientation.

--- sequenceur/faustwasm/index.js
+++ sequenceur/faustwasm/index.js
@@ -3746,9 +3746,12 @@ var FaustAudioWorkletNode = class extends (globalThis.AudioWorkletNode || null) 
         if (typeof window.DeviceMotionEvent.requestPermission === "function") {
           try {
             const response = await window.DeviceMotionEvent.requestPermission();
-            if (response !== "granted")
+            if (response === "granted") {
+              window.addEventListener("devicemotion", this.handleDeviceMotion, true);
+            } else if (response === "denied") {
+              alert("You have denied access to motion and orientation data. To enable it, go to Settings > Safari > Motion & Orientation Access.");
               throw new Error("Unable to access the accelerometer.");
-            window.addEventListener("devicemotion", this.handleDeviceMotion, true);
+            }
           } catch (error) {
             console.error(error);
           }
@@ -3764,9 +3767,12 @@ var FaustAudioWorkletNode = class extends (globalThis.AudioWorkletNode || null) 
         if (typeof window.DeviceOrientationEvent.requestPermission === "function") {
           try {
             const response = await window.DeviceOrientationEvent.requestPermission();
-            if (response !== "granted")
-              throw new Error("Unable to access the gyroscope.");
-            window.addEventListener("deviceorientation", this.handleDeviceOrientation, true);
+            if (response === "granted") {
+              window.addEventListener("deviceorientation", this.handleDeviceOrientation, true);
+            } else if (response === "denied") {
+              alert("You have denied access to motion and orientation data. To enable it, go to Settings > Safari > Motion & Orientation Access.");
+              throw new Error("Unable to access the accelerometer.");
+            }
           } catch (error) {
             console.error(error);
           }
