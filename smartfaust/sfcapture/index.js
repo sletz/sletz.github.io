2ef1fb423c5911face7c7d5e7a7eb801c107bd67
Stéphane Letz <letz@grame.fr>
2024-10-29 18:18:09 +0100

    Updated sfcapture.

--- smartfaust/sfcapture/index.js
+++ smartfaust/sfcapture/index.js
@@ -33,8 +33,8 @@ audioContext.suspend();
 
     const { createFaustNode } = await import("./create-node.js");
     // To test the ScriptProcessorNode mode
-    const { faustNode, dspMeta: { name } } = await createFaustNode(audioContext, "osc", FAUST_DSP_VOICES, true, 512);
-    //const { faustNode, dspMeta: { name } } = await createFaustNode(audioContext, "osc", FAUST_DSP_VOICES);
+    //const { faustNode, dspMeta: { name } } = await createFaustNode(audioContext, "osc", FAUST_DSP_VOICES, true, 512);
+    const { faustNode, dspMeta: { name } } = await createFaustNode(audioContext, "osc", FAUST_DSP_VOICES);
     if (!faustNode) throw new Error("Faust DSP not compiled");
 
     // Create the Faust UI
@@ -44,11 +44,13 @@ audioContext.suspend();
     // Connect the Faust node to the audio output
     faustNode.connect(audioContext.destination);
 
+    /*
     // Connect the Faust node to the audio input
     if (faustNode.numberOfInputs > 0) {
         const { connectToAudioInput } = await import("./create-node.js");
         await connectToAudioInput(audioContext, null, faustNode, null);
     }
+    */
 
     // Function to start MIDI
     function startMIDI() {
@@ -109,6 +111,12 @@ audioContext.suspend();
         if (!midiHandlersBound && FAUST_DSP_VOICES > 0) {
             startMIDI();
             midiHandlersBound = true;
+        }
+
+        // Connect the Faust node to the audio input
+        if (faustNode.numberOfInputs > 0) {
+            const { connectToAudioInput } = await import("./create-node.js");
+            await connectToAudioInput(audioContext, null, faustNode, null);
         }
     }
 
