ee029654086e5dd8a454eedb1bb272b2daffa66c
Stéphane Letz <letz@grame.fr>
2025-01-31 20:57:53 +0100

    Add versioning(2).

--- smartfaust/sfmoulin/service-worker.js
+++ smartfaust/sfmoulin/service-worker.js
@@ -5,7 +5,7 @@ const FAUST_DSP_VOICES = 0;
 // Set to true if the DSP has an effect
 const FAUST_DSP_HAS_EFFECT = false;
 
-const CACHE_NAME = "sfMoulin_20250131-2054"; // Cache name with versioning
+const CACHE_NAME = "sfMoulin_20250131-2056"; // Cache name with versioning
 
 /**
  * List of essential resources required for the **Mono DSP** version of the application.
