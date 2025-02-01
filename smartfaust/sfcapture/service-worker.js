6ab1547474424671ac63709e8eb7413b190b1726
Stéphane Letz <letz@grame.fr>
2025-02-01 11:59:57 +0100

    Add versioning(5).

--- smartfaust/sfcapture/service-worker.js
+++ smartfaust/sfcapture/service-worker.js
@@ -5,7 +5,7 @@ const FAUST_DSP_VOICES = 0;
 // Set to true if the DSP has an effect
 const FAUST_DSP_HAS_EFFECT = false;
 
-const CACHE_NAME = "sfCapture_20250201-1156"; // Cache name with versioning
+const CACHE_NAME = "sfCapture_20250201-1159"; // Cache name with versioning
 
 /**
  * List of essential resources required for the **Mono DSP** version of the application.
